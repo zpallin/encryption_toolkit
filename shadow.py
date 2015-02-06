d84d721000db094dac1aa99b8d78011a620ace62
Zeppelin <zpallin@gmail.com>
2015-02-05 16:17:45 -0800

    Shadow, a small script that returns an awesome /etc/shadow hash

---
+++ shadow.py
@@ -0,0 +1,18 @@
+
+#
+# Zeppelin Hash
+#   by Zpallin
+#
+#   Prints a highly randomized, secure password hash to be inserted into linux /etc/shadow files. 
+#   
+
+import crypt, getpass, random, string
+from passlib.hash import sha512_crypt
+
+salt_length = 13
+salt_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(salt_length))
+password_string = getpass.getpass()
+number_of_rounds = int(random.random() * 10000)
+
+# return password hash
+print sha512_crypt.encrypt(password_string, rounds=number_of_rounds, salt=salt_string)
