b829136da9c28d71306c25ea7e68f582127fb3b8
siteops+jenkins-server@reputation.com <zach@zach-mb.reputation.lan>
2017-08-02 10:02:03 -0700

    Update git script, get rid of git-new-branch, rm symlink
    - git new branch was not being loaded properly, somehow preventing
    logins to gnome desktop. Replaced with push.default set to matching
    - rm toolkit symlink

--- bash/includes/git.sh
+++ bash/includes/git.sh
@@ -5,18 +5,8 @@
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
-git-new-branch() {
-  NEW_BRANCH="$1"
-  IS_REPO=$(git -C ./ rev-parse > /dev/null 2>&1; echo $?)
+if type "git" > /dev/null 2>&1
+then
+  git config --global push.default "matching"
+fi
 
-  if [ -z $NEW_BRANCH ];
-  then
-    echo "Creates a new branch and pushes it to your origin automatically"
-  fi
-
-  if [ "$IS_REPO" == "0" ];
-  then
-    git checkout -b $1
-    git push origin $1 --set-upstream
-  fi
-}
